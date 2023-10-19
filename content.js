f1a59e88268b1c6743a3c7441b652ee00c0952c4
ziad <135853660+ziadomalik@users.noreply.github.com>
2023-10-19 02:07:12 +0200

    feat: do extension.

---
+++ content.js
@@ -0,0 +1,19 @@
+function checkForAd() {
+    const adElement = document.querySelector("div.ad-showing .video-stream");
+    if (adElement) {
+        adElement.playbackRate = 16;
+    } else {
+        const videoElement = document.querySelector(".video-stream");
+        if (videoElement) {
+            videoElement.playbackRate = 1;
+        }
+    }
+}
+
+checkForAd();
+
+const observer = new MutationObserver(checkForAd);
+observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+});
